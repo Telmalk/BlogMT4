6015e4578f11cbbb3fb6e2596f06238479ffcfdc
MosieurClement <clement.haller@noir.dev>
2021-07-27 12:41:35 +0200

    router

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -4,5 +4,26 @@ namespace Controller;
 
 class FrontController
 {
-
+    public function __construct()
+    {
+        \session_start();
+        $action = $_POST["action"] ?? $_GET["action"] ?? '';
+        switch ($action) {
+            case "article.show":
+                break;
+            case "article.delete":
+                break;
+            case "article.add":
+                break;
+            case "user.add":
+                break;
+            case "user.update":
+                break;
+            case "comment.add":
+                break;
+            case "article.index":
+                break;
+            default:
+        }
+    }
 }
