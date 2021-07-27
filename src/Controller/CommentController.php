1758c25af95fa3f9419cde0989ed8177453df8ba
christophe <ccharlebois@greenflex.com>
2021-07-27 12:48:18 +0200

    add CommentController

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Controller;
+
+
+class CommentController
+{
+    public function addComment()
+    {
+
+    }
+
+    public function getComment()
+    {
+
+    }
+
+    public function updateComment()
+    {
+
+    }
+
+    public function deleteComment()
+    {
+        
+    }
+}
